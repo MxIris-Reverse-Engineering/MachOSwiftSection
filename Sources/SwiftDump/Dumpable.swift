f88b37e69a4a3d030c6c9ba49c5ba58616f29d1e
Mx-Iris <mxiris@126.com>
2025-06-16 00:11:32 +0800

    Add NamedDumpable

--- Sources/SwiftDump/Dumpable.swift
+++ Sources/SwiftDump/Dumpable.swift
@@ -8,3 +8,8 @@ public protocol Dumpable {
     func dump(using options: DemangleOptions, in machOFile: MachOFile) throws -> SemanticString
     func dump(using options: DemangleOptions, in machOImage: MachOImage) throws -> SemanticString
 }
+
+public protocol NamedDumpable: Dumpable {
+    func dumpName(using options: DemangleOptions, in machOFile: MachOFile) throws -> SemanticString
+    func dumpName(using options: DemangleOptions, in machOImage: MachOImage) throws -> SemanticString
+}
