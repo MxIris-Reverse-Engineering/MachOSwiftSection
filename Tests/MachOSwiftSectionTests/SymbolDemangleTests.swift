97c7411a2c33185209be6895e199a8927ae746c3
Mx-Iris <mxiris@126.com>
2025-06-26 00:52:16 +0800

    Update Testing

--- Tests/MachOSwiftSectionTests/SymbolDemangleTests.swift
+++ Tests/MachOSwiftSectionTests/SymbolDemangleTests.swift
@@ -28,54 +28,53 @@ struct SymbolDemangleTests {
     @Test func symbols() throws {
         let allSwiftSymbols = try allSymbols()
         print("Total Swift Symbols: \(allSwiftSymbols.count)")
-        for symbol in allSwiftSymbols where symbol.stringValue.starts(with: "_$s") {
-            guard !symbol.stringValue.hasSuffix("$delayInitStub") else { continue }
-            var demangler = Demangler(scalars: symbol.stringValue.unicodeScalars)
-            let node = try demangler.demangleSymbol()
+        for symbol in allSwiftSymbols {
             let swiftStdlibDemangledName = stdlib_demangleName(symbol.stringValue)
-            let swiftSectionDemanlgedName = node.print()
-            #expect(swiftStdlibDemangledName == swiftSectionDemanlgedName, "\(symbol.stringValue)")
+            do {
+                guard !symbol.stringValue.hasSuffix("$delayInitStub") else { continue }
+                var demangler = Demangler(scalars: symbol.stringValue.unicodeScalars)
+                let node = try demangler.demangleSymbol()
+                let swiftSectionDemanlgedName = node.print()
+                #expect(swiftStdlibDemangledName == swiftSectionDemanlgedName, "\(symbol.stringValue)")
+            } catch {
+                #expect(symbol.stringValue == swiftStdlibDemangledName)
+                print(symbol)
+                print(error)
+            }
         }
     }
 
-    @Test func writeMangledNameToDesktop() async throws {
-        let symbols = try symbols(for: .AppKit, .SwiftUI, .SwiftUICore, .Foundation, .UIKitCore, .AttributeGraph)
-        let mangledNames = symbols.filter { $0.stringValue.starts(with: "_$s") }.map(\.stringValue).joined(separator: "\n")
-        try mangledNames.write(to: .desktopDirectory.appendingPathComponent("MangledSymbols.txt"), atomically: true, encoding: .utf8)
-    }
-
-    @MainActor
-    @Test func writeDemangledNameToDesktop() async throws {
-        let symbols = try symbols(for: .AppKit, .SwiftUI, .SwiftUICore, .Foundation, .UIKitCore, .AttributeGraph)
-        let demangledNames = try symbols.filter { $0.stringValue.starts(with: "_$s") }.map { symbol in
-            var demangler = Demangler(scalars: symbol.stringValue.unicodeScalars)
-            let node = try demangler.demangleSymbol()
-            return node.print(using: .interface)
-        }.joined(separator: "\n")
-        try demangledNames.write(to: .desktopDirectory.appendingPathComponent("DemangledSymbols.txt"), atomically: true, encoding: .utf8)
-    }
-
     @Test func demangle() async throws {
-        var demangler = Demangler(scalars: "_$s7SwiftUI22FinishLaunchTestActionV14callAsFunctionyyF".unicodeScalars)
+        var demangler = Demangler(scalars: "_$s6Charts10ChartProxyV5value2at2asx_q_tSgSo7CGPointV_x_q_tmtAA9PlottableRzAaJR_r0_lF".unicodeScalars)
         let node = try demangler.demangleSymbol()
         node.print().print()
     }
 
-    private func symbols(for machOImageNames: MachOImageName...) throws -> [MachOSymbol] {
-        try (machOImageNames.flatMap { try (required(mainCache.machOFile(named: $0))).symbols.map { MachOSymbol(offset: $0.offset, stringValue: $0.name) } }) +
-            (machOImageNames.flatMap { try (required(mainCache.machOFile(named: $0))).exportedSymbols.compactMap { symbol in symbol.offset.map { MachOSymbol(offset: $0, stringValue: symbol.name) } } })
+    private func symbols(for machOImageNames: MachOImageName...) throws -> [MachOSwiftSymbol] {
+        var symbols: [MachOSwiftSymbol] = []
+        for machOImageName in machOImageNames {
+            let machOFile = try required(mainCache.machOFile(named: machOImageName))
+            for symbol in machOFile.symbols where symbol.name.isSwiftSymbol {
+                symbols.append(MachOSwiftSymbol(imagePath: machOFile.imagePath, offset: symbol.offset, stringValue: symbol.name))
+            }
+            for symbol in machOFile.exportedSymbols where symbol.name.isSwiftSymbol {
+                if let offset = symbol.offset {
+                    symbols.append(MachOSwiftSymbol(imagePath: machOFile.imagePath, offset: offset, stringValue: symbol.name))
+                }
+            }
+        }
+
+        return symbols
     }
 
     private func allSymbols() throws -> [MachOSwiftSymbol] {
         var symbols: [MachOSwiftSymbol] = []
         for machOFile in Array(mainCache.machOFiles()) + Array(subCache.machOFiles()) {
-            for symbol in machOFile.symbols {
-                if symbol.name.isSwiftSymbol {
-                    symbols.append(MachOSwiftSymbol(imagePath: machOFile.imagePath, offset: symbol.offset, stringValue: symbol.name))
-                }
+            for symbol in machOFile.symbols where symbol.name.isSwiftSymbol {
+                symbols.append(MachOSwiftSymbol(imagePath: machOFile.imagePath, offset: symbol.offset, stringValue: symbol.name))
             }
-            for symbol in machOFile.exportedSymbols {
-                if let offset = symbol.offset, symbol.name.isSwiftSymbol {
+            for symbol in machOFile.exportedSymbols where symbol.name.isSwiftSymbol {
+                if let offset = symbol.offset {
                     symbols.append(MachOSwiftSymbol(imagePath: machOFile.imagePath, offset: offset, stringValue: symbol.name))
                 }
             }
@@ -84,8 +83,4 @@ struct SymbolDemangleTests {
     }
 }
 
-extension String {
-    var isSwiftSymbol: Bool {
-        getManglingPrefixLength(unicodeScalars) > 0
-    }
-}
+
