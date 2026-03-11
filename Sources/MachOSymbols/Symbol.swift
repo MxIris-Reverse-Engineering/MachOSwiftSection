f78af88f648b78241fd806f8973d31bfefc3e0c0
Mace <mxiris@private.com>
2026-03-11 18:18:55 +0800

    fix: pin branch dependencies to version tags and resolve demangledNode ambiguity
    - Pin MachOKit fork to 0.46.100, MachOObjCSection fork to 0.6.100
    - Pin FrameworkToolbox to 0.4.0, SourceKitD to 0.1.0, swift-apinotes to 0.1.0
    - Remove swift-power-assert branch dependency
    - Disable local path dependencies by default for reproducible builds
    - Comment out MachOKit.SymbolProtocol.demangledNode to resolve ambiguity with MachOSymbols.SymbolProtocol
    - Bump MachOKit version to 0.46.1, MachOObjCSection origin to 0.6.0

--- Sources/MachOSymbols/Symbol.swift
+++ Sources/MachOSymbols/Symbol.swift
@@ -75,13 +75,13 @@ extension MachOSymbols.SymbolProtocol {
     }
 }
 
-extension MachOKit.SymbolProtocol {
-    public var demangledNode: Node {
-        get throws {
-            try demangleAsNode(name)
-        }
-    }
-}
+//extension MachOKit.SymbolProtocol {
+//    public var demangledNode: Node {
+//        get throws {
+//            try demangleAsNode(name)
+//        }
+//    }
+//}
 
 extension MachOKit.ExportedSymbol: MachOSymbols.SymbolProtocol {}
 extension MachOKit.MachOFile.Symbol: MachOSymbols.SymbolProtocol {}
