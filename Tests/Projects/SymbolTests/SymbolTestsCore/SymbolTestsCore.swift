04208daf96f5814ef436bb108cab5e1a84af1acd
Mx-Iris <mxiris@126.com>
2026-04-10 17:49:13 +0800

    feat: add new test types to SymbolTestsCore for attribute metadata parsing
    Add PropertyWrapperStruct, ResultBuilderStruct, DynamicMemberLookupStruct, DynamicCallableStruct, and ObjCAttributeClass to expand test coverage for Swift attribute metadata extraction.

--- Tests/Projects/SymbolTests/SymbolTestsCore/SymbolTestsCore.swift
+++ Tests/Projects/SymbolTests/SymbolTestsCore/SymbolTestsCore.swift
@@ -350,6 +350,43 @@ public enum MultiPayloadEnumTests2 {
 public struct GenericNestedFunction<A, B> {
     let a: A
     let b: B
-    
+
     public func function<A1, B1>(a: A1, b: B1) {}
+}
+
+@propertyWrapper
+public struct PropertyWrapperStruct<Value: Comparable> {
+    public var wrappedValue: Value
+    public var projectedValue: ClosedRange<Value>
+
+    public init(wrappedValue: Value, range: ClosedRange<Value>) {
+        self.projectedValue = range
+        self.wrappedValue = min(max(wrappedValue, range.lowerBound), range.upperBound)
+    }
+}
+
+@resultBuilder
+public struct ResultBuilderStruct<Element> {
+    public static func buildBlock(_ components: Element...) -> [Element] { components }
+    public static func buildOptional(_ component: [Element]?) -> [Element] { component ?? [] }
+}
+
+@dynamicMemberLookup
+public struct DynamicMemberLookupStruct {
+    public subscript(dynamicMember member: String) -> Int { 0 }
+}
+
+@dynamicCallable
+public struct DynamicCallableStruct {
+    public func dynamicallyCall(withArguments arguments: [Int]) -> Int {
+        arguments.reduce(0, +)
+    }
+
+    public func dynamicallyCall(withKeywordArguments arguments: KeyValuePairs<String, Int>) -> Int { 0 }
+}
+
+public class ObjCAttributeClass: NSObject {
+    @objc public func objcMethod() {}
+    @nonobjc public func nonobjcMethod() {}
+    @objc public dynamic func objcDynamicMethod() {}
 }
