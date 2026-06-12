74616ae4a167b91a6d24693936b3281f81e50546
Mx-Iris <mxiris@126.com>
2026-06-12 18:17:50 +0800

    refactor(swift-section): drop offset-comment flags from dump command
    Remove --emit-offset-comments and --emit-expanded-field-offsets from
    the CLI surface and rename emitPwtAddresses to emitPWTAddresses for
    consistent acronym casing.

--- Sources/swift-section/Commands/DumpCommand.swift
+++ Sources/swift-section/Commands/DumpCommand.swift
@@ -56,20 +56,14 @@ struct DumpCommand: AsyncParsableCommand, Sendable {
     @Option(name: .shortAndLong, help: "The color scheme for the output.")
     var colorScheme: SemanticColorScheme = .none
 
-    @Flag(help: "Generate field offset and PWT offset comments, if possible")
-    var emitOffsetComments: Bool = false
-
     @Flag(help: "Generate member address comments for each member symbol")
     var emitMemberAddresses: Bool = false
 
     @Flag(help: "Generate vtable offset comments for class methods")
     var emitVtableOffsets: Bool = false
 
-    @Flag(help: "Expand nested struct fields with their absolute offsets (implies --emit-offset-comments)")
-    var emitExpandedFieldOffsets: Bool = false
-
     @Flag(help: "Generate PWT (Protocol Witness Table) address comments for protocol conformances")
-    var emitPwtAddresses: Bool = false
+    var emitPWTAddresses: Bool = false
 
     @Flag(help: "The definitions of types and protocols will be output in the order they are stored in the binary.")
     var preferredBinaryOrder: Bool = false
@@ -79,13 +73,9 @@ struct DumpCommand: AsyncParsableCommand, Sendable {
 
         var dumpConfiguration: DumperConfiguration = .demangleOptions(demangleOptions.buildSwiftDumpDemangleOptions())
 
-        let effectiveEmitOffsetComments = emitOffsetComments || emitExpandedFieldOffsets
-
-        dumpConfiguration.printFieldOffset = effectiveEmitOffsetComments
-        dumpConfiguration.printExpandedFieldOffsets = emitExpandedFieldOffsets
         dumpConfiguration.printMemberAddress = emitMemberAddresses
         dumpConfiguration.printVTableOffset = emitVtableOffsets
-        dumpConfiguration.printConformancePWTAddress = emitPwtAddresses
+        dumpConfiguration.printConformancePWTAddress = emitPWTAddresses
 
         let isDefaultSections = sections.isEmpty
 
